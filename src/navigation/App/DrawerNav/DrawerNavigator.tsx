c7325b8b7cba4e6a180ad223fa53a51d38e271ca
yash <yashrajsinhjadeja2253@gmail.com>
2026-04-27 18:56:51 +0530

    Revert "UI polished"
    This reverts commit 4ec736a716fb2097591da45e2dc1790bffe1e805.

--- src/navigation/App/DrawerNav/DrawerNavigator.tsx
+++ src/navigation/App/DrawerNav/DrawerNavigator.tsx
@@ -51,14 +51,14 @@ function CustomDrawerContent(props: DrawerContentComponentProps) {
     <Container>
       <HeaderImage
         source={{
-          uri: 'https://wallpapercave.com/wp/wp2043918.jpg',
+          uri: 'https://images.alphacoders.com/241/thumb-1920-241901.jpg',
         }}
         resizeMode="cover"
       />
 
       <DrawerContentScrollView
         {...props}
-        contentContainerStyle={{ paddingTop: 5 }}
+        contentContainerStyle={{ paddingTop: 0 }}
         showsVerticalScrollIndicator={false}
       >
         {renderItem('Home', 'grid-outline', 'Home')}
--- src/styles/drawer.style.ts
+++ src/styles/drawer.style.ts
@@ -11,20 +11,20 @@ export const Container = styled.View`
 
 export const HeaderImage = styled.Image`
   width: 100%;
-  height: 250px;
+  height: 220px;
 `;
 export const ItemWrapper = styled.TouchableOpacity<ItemProps>`
   flex-direction: row;
   align-items: center;
   padding: 14px 16px;
-  margin: 0px;
-  border-radius: 30px;
+  margin: 0px 1px 1px;
+  border-radius: 20px;
   background-color: ${({ active }) =>
     active ? 'rgba(79,70,229,0.12)' : 'transparent'};
 `;
 
 export const ItemLabel = styled.Text<ItemProps>`
-  margin-left: 10px;
+  margin-left: 12px;
   font-size: 14px;
   font-weight: 600;
   color: ${({ active }) => (active ? '#4F46E5' : '#1e293b')};
